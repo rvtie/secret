316fba2d5c25337a759c638a6fd8b551ad693577
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2014-08-29 22:57:52 +0530

    added script to get evaluation

--- evals.py
+++ evals.py
@@ -4,10 +4,10 @@ from SIG.models import SIGroup
 
 def run():
 	passed=models.Resume.objects.filter(qualified_for_round=3).order_by('id')
-	model = models.Resume
-	codeWriter = csv.writer(open('codeQualifiedGD.csv','w'))
-	garageWriter = csv.writer(open('garageQualifiedGD.csv','w'))
-	gadgetWriter = csv.writer(open('gadgetQualifiedGD.csv','w'))
+        model = models.Resume
+	codeWriter = csv.writer(open('codeQualifiedGDEvals.csv','w'))
+	garageWriter = csv.writer(open('garageQualifiedGDEvals.csv','w'))
+	gadgetWriter = csv.writer(open('gadgetQualifiedGDEvals.csv','w'))
 	headers = []
 	for field in model._meta.fields:
 		headers.append(field.name)
@@ -19,10 +19,10 @@ def run():
 	garage=SIGroup.objects.get(name="Garage")
 	for resume in passed:
 		row=[]
-		codeChoice=False
-		gadgetChoice=False
-		garageChoice=False
-		for field in resumeHeaders:
+		PIeval=resume.resume.filter(current_round="Group Discussion")
+                if PIeval is None:
+                    print "This isnt working."
+                for evaluation in PIeval:
 			val=getattr(resume, field)
 			if callable(val):
 				val=val()
